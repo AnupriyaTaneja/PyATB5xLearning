479589c860a488b70c62aadc92d64527a1da6165
AnupriyaTaneja <anupriya.taneja@gmail.com>
2024-11-17 02:51:51 +0530

    Nov9_tasks

---
+++ Nov/ex09112024_Literals_Variables/Lab_030_Task2.py
@@ -0,0 +1,24 @@
+# Task for the Today
+# Take a 3 input from the user
+# perform the sub, sub, mul and div
+
+num1 = input("Enter the value of num1")
+num2 = input("Enter the value of num2")
+num3 = input("Enter the value of num3")
+num1 = int(num1)
+num2 = int(num2)
+num3 = int(num3)
+print("Sum: ", num1+num2+num3)
+print("Sub: ", num1-num2-num3)
+print("Mul: ", num1*num2*num3)
+print("Div: ", num1/num2/num3)
+
+"""Output:
+Enter the value of num11000
+Enter the value of num2100
+Enter the value of num310
+Sum:  1110
+Sub:  890
+Mul:  1000000
+Div:  1.0
+"""
